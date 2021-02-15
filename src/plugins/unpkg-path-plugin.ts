94e8e7c803b418d1a5d1127f46e9ec1d4031c5c5
yaasin-raki2 <yassin23raki@gmail.com>
2021-02-15 13:40:26 +0100

    adding the ability to check for other files inside the package and join them to the base url using the URL api

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -9,12 +9,26 @@ export const unpkgPathPlugin = () => {
         console.log("onResolve", args);
         if (args.path === "index.js") {
           return { path: args.path, namespace: "a" };
-        } else if (args.path === "tiny-test-pkg") {
+        }
+
+        if (args.path.includes("./") || args.path.includes("../")) {
           return {
-            path: "https://unpkg.com/tiny-test-pkg@1.0.0/index.js",
             namespace: "a",
+            path: new URL(args.path, args.importer + "/").href,
           };
         }
+
+        return {
+          namespace: "a",
+          path: `https://unpkg.com/${args.path}`,
+        };
+
+        // else if (args.path === "tiny-test-pkg") {
+        //   return {
+        //     path: "https://unpkg.com/tiny-test-pkg@1.0.0/index.js",
+        //     namespace: "a",
+        //   };
+        // }
       });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
@@ -24,7 +38,7 @@ export const unpkgPathPlugin = () => {
           return {
             loader: "jsx",
             contents: `
-              const message = require('tiny-test-pkg');
+              const message = require('medium-test-pkg');
               console.log(message);
             `,
           };
