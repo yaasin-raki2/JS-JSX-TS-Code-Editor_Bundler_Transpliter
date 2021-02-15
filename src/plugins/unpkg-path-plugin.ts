cc647f81cfc16e9036bd198c46b8d7be1a81ae66
yaasin-raki2 <yassin23raki@gmail.com>
2021-02-15 16:13:38 +0100

    It works !

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -14,7 +14,7 @@ export const unpkgPathPlugin = () => {
         if (args.path.includes("./") || args.path.includes("../")) {
           return {
             namespace: "a",
-            path: new URL(args.path, args.importer + "/").href,
+            path: new URL(args.path, "https://unpkg.com" + args.resolveDir + "/").href,
           };
         }
 
@@ -22,13 +22,6 @@ export const unpkgPathPlugin = () => {
           namespace: "a",
           path: `https://unpkg.com/${args.path}`,
         };
-
-        // else if (args.path === "tiny-test-pkg") {
-        //   return {
-        //     path: "https://unpkg.com/tiny-test-pkg@1.0.0/index.js",
-        //     namespace: "a",
-        //   };
-        // }
       });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
@@ -38,17 +31,17 @@ export const unpkgPathPlugin = () => {
           return {
             loader: "jsx",
             contents: `
-              const message = require('medium-test-pkg');
-              console.log(message);
+            import react, { useState } from "react"
+            console.log(react, useState);
             `,
           };
         }
 
-        const { data } = await axios.get(args.path);
-
+        const { data, request } = await axios.get(args.path);
         return {
           loader: "jsx",
           contents: data,
+          resolveDir: new URL("./", request.responseURL).pathname,
         };
       });
     },
