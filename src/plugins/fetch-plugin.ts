3c1da6b8be9045b9212028bd10c143795cb27127
yaasin-raki2 <yassin23raki@gmail.com>
2021-02-16 17:36:20 +0100

    More Refactoring

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -10,20 +10,41 @@ export const fetchPlugin = (inputCode: string) => {
   return {
     name: "fetch-plugin",
     setup(build: esbuild.PluginBuild) {
-      build.onLoad({ filter: /.*/ }, async (args: any) => {
-        if (args.path === "index.js") {
-          return {
-            loader: "jsx",
-            contents: inputCode,
-          };
-        }
+      build.onLoad({ filter: /(^index\.js$)/ }, () => {
+        return {
+          loader: "jsx",
+          contents: inputCode,
+        };
+      });
 
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
         const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
 
         if (cachedResult) {
           return cachedResult;
         }
+      });
 
+      build.onLoad({ filter: /.css$/ }, async (args: any) => {
+        const { data, request } = await axios.get(args.path);
+        const escaped = data.replace(/\n/g, "").replace(/"/g, '\\"').replace(/'/g, "\\'");
+        const contents = `
+          const style = document.createElement('style');
+          style.innerText = '${escaped}';
+          document.head.appendChild(style);
+        `;
+
+        const result: esbuild.OnLoadResult = {
+          loader: "jsx",
+          contents,
+          resolveDir: new URL("./", request.responseURL).pathname,
+        };
+        await fileCache.setItem(args.path, result);
+
+        return result;
+      });
+
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
         const { data, request } = await axios.get(args.path);
 
         const result: esbuild.OnLoadResult = {
@@ -31,7 +52,6 @@ export const fetchPlugin = (inputCode: string) => {
           contents: data,
           resolveDir: new URL("./", request.responseURL).pathname,
         };
-
         await fileCache.setItem(args.path, result);
 
         return result;
