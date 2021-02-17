31258fe5f726a74cb363fdba860f0c0d94acfd55
yaasin-raki2 <yassin23raki@gmail.com>
2021-02-17 17:31:19 +0100

    Installing Monaco-Editor/React and wiring it up as a component to the index.tsx file

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom";
 
 import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 import { fetchPlugin } from "./plugins/fetch-plugin";
+import CodeEditor from "./components/code-editor";
 
 const App = () => {
   const [input, setInput] = useState("");
@@ -63,6 +64,7 @@ const App = () => {
 
   return (
     <div>
+      <CodeEditor />
       <textarea value={input} onChange={(e) => setInput(e.target.value)}></textarea>
       <div>
         <button onClick={onClick}>Submit</button>
