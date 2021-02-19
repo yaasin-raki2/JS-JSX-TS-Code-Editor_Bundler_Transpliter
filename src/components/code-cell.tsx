9e1a2f9d65013688e9811b37c7c342bc9e2dde4c
yaasin-raki2 <yassin23raki@gmail.com>
2021-02-19 19:30:57 +0100

    Adding logic to excecute code after 750ms when the user stop typing

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { OnChange } from "@monaco-editor/react";
 
 import CodeEditor from "./code-editor";
@@ -10,10 +10,16 @@ const CodeCell = () => {
   const [input, setInput] = useState("");
   const [code, setCode] = useState("");
 
-  const onClick = async () => {
-    const output = await bundle(input);
-    setCode(output);
-  };
+  useEffect(() => {
+    const timer = setTimeout(async () => {
+      const output = await bundle(input);
+      setCode(output);
+    }, 750);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [input]);
 
   const onChange: OnChange = (value) => value && setInput(value);
 
