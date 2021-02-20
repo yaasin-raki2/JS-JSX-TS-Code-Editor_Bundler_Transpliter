a8b5cb1c062146eeaa8c1a8654f08ed9879d6071
yaasin-raki2 <yassin23raki@gmail.com>
2021-02-20 13:11:52 +0100

    Creating a redux store , Handling all the sync actions creators, and wiring up redux to the app

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -18,6 +18,19 @@ const initialState: CellsState = {
   data: {},
 };
 
-const reducer = (state: CellsState = initialState, action: Action): CellsState => {};
+const reducer = (state: CellsState = initialState, action: Action): CellsState => {
+  switch (action.type) {
+    case ActionType.UPDATE_CELL:
+      return state;
+    case ActionType.DELETE_CELL:
+      return state;
+    case ActionType.MOVE_CELL:
+      return state;
+    case ActionType.INSERT_CELL_BEFORE:
+      return state;
+    default:
+      return state;
+  }
+};
 
 export default reducer;
