907d1f6f8d98d2d67bd901ab7b2093cba8b17c3d
yaasin-raki2 <yassin23raki@gmail.com>
2021-02-20 21:05:23 +0100

    Creating an Action Bar with some fontAwesome icons

--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -1,6 +1,9 @@
+import "./cell-list-item.css";
+
 import { Cell } from "../state";
 import CodeCell from "./code-cell";
 import TextEditor from "./text-editor";
+import ActionBar from "./action-bar";
 
 interface CellListItemProps {
   cell: Cell;
@@ -8,8 +11,20 @@ interface CellListItemProps {
 
 const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
   return (
-    <div>
-      {cell.type === "code" ? <CodeCell cell={cell} /> : <TextEditor cell={cell} />}
+    <div className="cell-list-item">
+      {cell.type === "code" ? (
+        <>
+          <div className="action-bar-wrapper">
+            <ActionBar id={cell.id} />
+          </div>
+          <CodeCell cell={cell} />
+        </>
+      ) : (
+        <>
+          <TextEditor cell={cell} />
+          <ActionBar id={cell.id} />
+        </>
+      )}
     </div>
   );
 };
